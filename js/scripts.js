6bddc67ac1d885142a49ffb95954a9797902b4c9
William Donovan-Seid <wdstwo@live.com>
2020-08-21 14:14:01 -0700

    add alert if user inputs non-number and try to get reverse button to work

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,41 @@ $(document).ready(function() {
     let tempArray = [];
     let finalArray = [];
 
-    for (i=0; i<=num; i++) {
-      let iString = i.toString();
-      numArray.push(iString);
-    }
+    if (!num) {
+      alert("Please enter a number");
+    } else {
+      
+      for (i=0; i<=num; i++) {
+        let iString = i.toString();
+        numArray.push(iString);
+      }
 
-    numArray.forEach(function(number) {
-      tempArray = number.split("");
-      if (tempArray.includes("3")) {
-        finalArray.push(" Won't you be my neighbor?")
-      } else if (tempArray.includes("2")) {
-        finalArray.push(" Boop!");
-      } else if (tempArray.includes("1")) {
-        finalArray.push(" Beep!");
-      } else {
-        finalArray.push(" "+number)
-      }
-      if (number === "7") {
-        $("body").addClass("seven");
-      } 
-      if (number === "42") {
-        $("body").addClass("hitchHiker");
-      }
-    });
-    $("#answer").show();
-    $("#answer").text(finalArray);
+      numArray.forEach(function(number) {
+        tempArray = number.split("");
+        if (tempArray.includes("3")) {
+          finalArray.push(" Won't you be my neighbor?")
+        } else if (tempArray.includes("2")) {
+          finalArray.push(" Boop!");
+        } else if (tempArray.includes("1")) {
+          finalArray.push(" Beep!");
+        } else {
+          finalArray.push(" "+number)
+        }
+        if (number === "7") {
+          $("body").addClass("seven");
+        } else if (number === "42") {
+          $("body").addClass("hitchHiker");
+        } else {
+          $("body").removeClass("seven hitchHiker");
+        }
+      });
+      $("#answer").show();
+      $("#answer").text(finalArray);
+      $("#reverse").click(function() {
+        const reverseArray = finalArray.reverse();
+        $("#answer").text(reverseArray);
+      });
+    };
   });
   $("button#originalBackground").click(function() {
     $("body").removeClass();
