f9b510ea4055c65252cfa7cc3eec6c1bb7238e22
William Donovan-Seid <wdstwo@live.com>
2020-08-21 10:26:34 -0700

    rework code for it to work with numbers higher then 10 for the 1st and 2nd spec only

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,27 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     
-    let num = parseInt($("#input").val());
+    const check = ["1"];
+    const num = parseInt($("#input").val());
     let numArray = [];
     let finalArray = [];
+    let tempArray = [];
+
     for (i=0; i<=num; i++) {
       let temp = i.toString();
       numArray.push(temp);
     }
 
     numArray.forEach(function(number) {
-      finalArray.push(number.replace(/1/g, "why"));
+	    tempArray=number.split("");
+      for (const tempNum of tempArray) {
+  	    if (check.includes(tempNum)) {
+    	    finalArray.push("Beep!");
+          break;
+  	    } else {
+    	    finalArray.push(number)
+  	    }
+      }
     });
 
     $("#answer").text(finalArray);
